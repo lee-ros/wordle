c9e27c492ada2185e1353c3cbfaf6dc197673988
Lee Rosenberg <laluk321@gmail.com>
2024-04-25 00:35:01 -0400

    initial migration

--- backend/app/alembic/env.py
+++ backend/app/alembic/env.py
@@ -8,6 +8,8 @@ from sqlalchemy import pool
 from alembic import context
 from dotenv import find_dotenv, load_dotenv
 
+from app import models
+
 
 load_dotenv(find_dotenv())
 
@@ -25,7 +27,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = models.Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
