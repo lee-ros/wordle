5d842aac4c9e05679e4f822f7a6a75dba9975e53
Lee Rosenberg <laluk321@gmail.com>
2024-04-26 14:48:50 -0400

    add header

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div class="flex-row align-middle mt-10">
+      <h1 class="text-5xl font-bold">The Great Wordle Game</h1>
+    </div>
+  );
+}
+
+export default Header;
