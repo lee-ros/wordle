46e00dcab917f72911c3ca397b0b2c3c66c464e4
Lee Rosenberg <laluk321@gmail.com>
2024-04-27 11:27:15 -0400

    move error handiling externally

--- frontend/src/client/requests.js
+++ frontend/src/client/requests.js
@@ -4,41 +4,25 @@ const API_URL = "http://localhost:8000/api/v1";
 
 export async function createGame(guess_limit, word_length) {
   const game_create = { guess_limit, word_length };
-  try {
-    const response = await axios.post(`${API_URL}/game`, game_create);
-    return await response.data;
-  } catch {
-    return null;
-  }
+  const response = await axios.post(`${API_URL}/game`, game_create);
+  return await response.data;
 }
 
 export async function fetchGame(game_id) {
-  try {
-    const response = await axios.get(`${API_URL}/game/${game_id}`);
-    return await response.data;
-  } catch {
-    return null;
-  }
+  const response = await axios.get(`${API_URL}/game/${game_id}`);
+  return await response.data;
 }
 
 export async function fetchGames() {
-  try {
-    const response = await axios.get(`${API_URL}/game`);
-    return await response.data;
-  } catch {
-    return null;
-  }
+  const response = await axios.get(`${API_URL}/game`);
+  return await response.data;
 }
 
 export async function createGuess(value, game_id) {
   const guess_create = { value };
-  try {
-    const response = await axios.patch(
-      `${API_URL}/game/${game_id}`,
-      guess_create
-    );
-    return await response.data;
-  } catch {
-    return null;
-  }
+  const response = await axios.patch(
+    `${API_URL}/game/${game_id}`,
+    guess_create
+  );
+  return await response.data;
 }
