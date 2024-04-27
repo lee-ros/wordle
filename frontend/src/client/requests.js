bcef657e53ff52e269886a53f1f10e2548f65a4b
Lee Rosenberg <laluk321@gmail.com>
2024-04-27 16:14:18 -0400

    add game delete client function

--- frontend/src/client/requests.js
+++ frontend/src/client/requests.js
@@ -2,15 +2,19 @@ import axios from "axios";
 
 const API_URL = "http://localhost:8000/api/v1";
 
-export async function createGame(guess_limit, word_length) {
-  const game_create = { guess_limit, word_length };
+export async function createGame(guessLimit, wordLength) {
+  const game_create = { guess_limit: guessLimit, word_length: wordLength };
   const response = await axios.post(`${API_URL}/game`, game_create);
   return await response.data;
 }
 
-export async function fetchGame(game_id) {
-  const response = await axios.get(`${API_URL}/game/${game_id}`);
+export async function fetchGame(gameId) {
+  const response = await axios.get(`${API_URL}/game/${gameId}`);
   return await response.data;
+}
+
+export async function deleteGame(gameId) {
+  await axios.delete(`${API_URL}/game/${gameId}`)
 }
 
 export async function fetchGames() {
@@ -18,10 +22,10 @@ export async function fetchGames() {
   return await response.data;
 }
 
-export async function createGuess(value, game_id) {
+export async function createGuess(value, gameId) {
   const guess_create = { value };
   const response = await axios.patch(
-    `${API_URL}/game/${game_id}`,
+    `${API_URL}/game/${gameId}`,
     guess_create
   );
   return await response.data;
