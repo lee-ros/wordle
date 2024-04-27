2a19b9299b7c714467325e7b139367c5a1bbeba9
Lee Rosenberg <laluk321@gmail.com>
2024-04-27 17:27:55 -0400

    clean

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,44 +10,15 @@ export const GameContext = createContext({
   setGame: () => {},
 });
 
-const empty_game = {
-  id: -1,
-  word_to_guess: "",
-  state: "",
-  guess_limit: -1,
-  guesses: [],
-};
-
-const mock_game = {
-  id: 1,
-  word_to_guess: "world",
-  state: "running",
-  guess_limit: 5,
-  guesses: [
-    {
-      value: "hello",
-      coloring_scheme: ["no_match", "no_match", "no_match", "in_place", "wrong_place"]
-    },
-    {
-      value: "hello",
-      coloring_scheme: ["no_match", "no_match", "no_match", "in_place", "wrong_place"]
-    },
-  ],
-};
-
 function App() {
-  const [game, setGame] = useState(mock_game);
+  const [game, setGame] = useState({});
 
   return (
     <GameContext.Provider value={{ game, setGame }}>
       <div className="App select-none">
         <Header />
         <div className="flex justify-center w-[75wh]">
-          {game.state === "running" ? (
-            <GuessFrame />
-          ) : (
-            <GamesFrame />
-          )}
+          {game?.state === "running" ? <GuessFrame /> : <GamesFrame />}
         </div>
       </div>
     </GameContext.Provider>
